04a2affb6171b6592ee3a96a216b05cf61ad797f
InSeong-So <goflvhxj2547@gmail.com>
2019-01-30 15:44:12 +0900

    카카오코딩테스트 대비 알고리즘 추가

--- src/main/java/algorithm/kakao/_01_Solution.java
+++ src/main/java/algorithm/kakao/_01_Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class _01_Solution {
 
 	// 오픈톡 : 첫번째, 해결 영상을 참조한 것.
 	public String[] solution(String[] record) {
