139fa28182a40fbe436630c7e75952dce37a5dc7
InSeong-So <goflvhxj2547@gmail.com>
2019-05-02 22:56:00 +0900

    다른 사람 풀이 추가

---
+++ src/main/java/practice/programmers/stack/탑.java
@@ -0,0 +1,17 @@
+package practice.programmers.stack;
+
+public class 탑 {
+	public int[] solution(int[] heights) {
+		int[] answer = new int[heights.length];
+
+		for (int i = 0; i < heights.length; i++) {
+			for (int j = i + 1; j < heights.length; j++) {
+				if (heights[i] > heights[j]) {
+					answer[j] = i + 1;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
