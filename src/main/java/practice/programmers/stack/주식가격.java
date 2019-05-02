69c41bf32972ebcb2f1f5a1c99bd89c632ee9138
InSeong-So <goflvhxj2547@gmail.com>
2019-05-02 22:56:00 +0900

    다른 사람 풀이 추가

---
+++ src/main/java/practice/programmers/stack/주식가격.java
@@ -0,0 +1,26 @@
+package practice.programmers.stack;
+
+import java.util.Stack;
+
+public class 주식가격 {
+	public int[] solution(int[] prices) {
+		Stack<Integer> beginIdxs = new Stack<>();
+		int i = 0;
+		int[] terms = new int[prices.length];
+
+		beginIdxs.push(i);
+		for (i = 1; i < prices.length; i++) {
+			while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+				int beginIdx = beginIdxs.pop();
+				terms[beginIdx] = i - beginIdx;
+			}
+			beginIdxs.push(i);
+		}
+		while (!beginIdxs.empty()) {
+			int beginIdx = beginIdxs.pop();
+			terms[beginIdx] = i - beginIdx - 1;
+		}
+
+		return terms;
+	}
+}
