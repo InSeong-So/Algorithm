2141aa64388c37da3410a88da121702409a28f46
InSeong-So <goflvhxj2547@gmail.com>
2020-10-08 11:26:56 +0900

    알고리즘 공부 + 포맷터 적용

--- package/java/algorithm/src/main/java/practice/programmers/greedy/구명보트.java
+++ package/java/algorithm/src/main/java/practice/programmers/greedy/구명보트.java
@@ -2,14 +2,17 @@ package practice.programmers.greedy;
 
 import java.util.Arrays;
 
-public class 구명보트 {
-	   public int solution(int[] people, int limit) {
-	        Arrays.sort(people);
-	        int i = 0, j = people.length - 1;
-	        for (; i < j; --j) {
-	            if (people[i] + people[j] <= limit)
-	                ++i;
-	        }
-	        return people.length - i;
-	    }
+public class 구명보트
+{
+    public int solution(int[] people, int limit)
+    {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j)
+        {
+            if (people[i] + people[j] <= limit)
+                ++i;
+        }
+        return people.length - i;
+    }
 }
