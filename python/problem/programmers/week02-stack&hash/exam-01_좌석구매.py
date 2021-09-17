c0b1b871bfcb32b858788cb669d62ac5c93437de
InSeong-So <goflvhxj2547@gmail.com>
2021-09-17 23:36:49 +0900

    ✒️Remove: python-programmers

--- python/problem/programmers/week02-stack&hash/exam-01_좌석구매.py
+++
@@ -1,8 +0,0 @@
-def solution(seat):
-    
-    answer = set([])
-    
-    for item in seat:
-        answer.add("-".join([str(_) for _ in item]))
-    
-    return len(answer)
--- python/problem/programmers/week04-sorting&dynamic/02_최솟값만들기.py
+++
@@ -1,5 +0,0 @@
-def solution(A, B):
-    A.sort()
-    B.sort(reverse=True)
-
-    return sum(a * b for a, b in zip(A, B))
